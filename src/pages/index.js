83241f3428c60d21f21a887b323994fdde99b80d
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-07-06 17:20:02 -0300

    Intial Structure

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>main Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Main
