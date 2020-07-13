b089dcf2175a78ba37a73fbf5b30bb22266c834f
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-07-13 20:01:45 -0300

    styled-media-query added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,12 @@ import { NavLink, Link } from 'react-router-dom'; // ¡¡
 import styled from 'styled-components';// ¡¡
 import { Icon } from 'react-icons-kit';
 
-import {ic_keyboard_arrow_right} from 'react-icons-kit/md/ic_keyboard_arrow_right'
+import {ic_keyboard_arrow_right} from 'react-icons-kit/md/ic_keyboard_arrow_right';
 
-import { Button } from './Button'
+import { Button } from './Button';
+
+// Media Query
+import { generateMedia } from "styled-media-query";
 
 
 class Header extends Component {
@@ -21,9 +24,9 @@ class Header extends Component {
                 <div className="header-content">
                     <Title>See what's next</Title>
                     <Subtitle>WATCH ANYWHERE, CANCEL ANYTIME.</Subtitle>
-                    <Button primary> 
+                    <Button className="main-offer-btn" primary> 
                         try it now
-                        <Icon className="iconbtn" icon={ic_keyboard_arrow_right} size={37} />
+                        <Icon className="Icon" icon={ic_keyboard_arrow_right} size={37} />
                     </Button>
                 </div>
             </HeaderComponent> 
@@ -33,6 +36,14 @@ class Header extends Component {
 
 export default Header;
 
+const customMedia = generateMedia({
+    lgDesktop: '1350px',
+    mdDesktop: '1150px',
+    tablet: '960px',
+    smTablet: '740px'
+})
+
+
 //Logo
 const Logo = styled.img`
     width: 10rem;
@@ -40,13 +51,27 @@ const Logo = styled.img`
     position: absolute;
     top: 25%;
     left: 50%;
-    transform: translate (-50%, -50%);
+    transform: translate(-50%, -50%);
+    margin-left: 0;
+    margin-top 2rem;
+    ${customMedia.lessThan('tablet')`
+        left:20%
+    `}
 `;
 
 
 // Header Container
-const HeaderComponent = styled.div`
-    .signIn-btn{
+const HeaderComponent = styled.header`
+
+.Icon svg {
+    vertical-align: bottom !important;
+    margin-left: 1.5rem;
+    ${customMedia.lessThan('smTablet')`
+        display: none !important;
+    `}
+}
+
+    .signIn-btn {
         right: 0;
         margin 1.125rem 3% 0;
         padding: 0.4375rem 1.0625rem;
@@ -60,12 +85,15 @@ const HeaderComponent = styled.div`
         cursor: pointer;
         transition: background 0.2s ease-in;
         &:hover {
-            background: var(--main-red-hover);
+            background: #d30913;
         }
+        ${customMedia.lessThan('smTablet')`
+            margin-top: 1.25rem;
+            right: 5%;
+        `}
     }
 
 // Header top
-
     .header-top {
         position: relative;
         height: 3rem;
@@ -74,7 +102,7 @@ const HeaderComponent = styled.div`
 
 //Header content
     .header-content {
-        width:65%;
+        width: 65%;
         position: relative;
         margin: 4.5rem auto 0;
         display: flex;
@@ -82,32 +110,50 @@ const HeaderComponent = styled.div`
         align-content: center;
         text-align: center;
         flex-direction: column;
-        z-index: 1;
+        z-index: 2;
+        ${customMedia.lessThan('smTablet')`
+            display: grid;
+
+        `}
     }
 
-
-    .iconbtn{
-        display: inline-block;
-        position: absolute;
+    .main-offer-btn{
+        ${customMedia.lessThan('lgDesktop')`
+            margin: 0 33%;
+            font-size: 1.5rem;
+        `}
+            ${customMedia.lessThan('mdDesktop')`
+            margin: 0 25%;
+            font-size: 1.5rem;
+        `}
+            ${customMedia.lessThan('tablet')`
+            margin: 0 20%;
+            font-size: 1.3rem;
+        `}
     }
-`;
+ `;
 
 //Main TItle
-
 const Title = styled.h1`
     margin: 0 0 1.rem;
     font-size:5rem;
     font-weight: 700;
     line-height: 1.1em;
+    ${customMedia.lessThan('tablet')`
+        font-size: 2.6rem;
+    `}
 `;
 
 
 //subtitle
-
 const Subtitle = styled.h2`
     font-weight: 400;
     font-size: 1.875rem;
     line-height:1.25rem;
     margin:0 0 1.875rem;
-    text-transform: uppercase;    
+    text-transform: uppercase;   
+    ${customMedia.lessThan('smTablet')`
+    font-size: 1.4rem;
+    margin-bottom: 10px;
+`} 
 `;
